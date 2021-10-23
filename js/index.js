7d39e63ffd9456ca17521d4c2b0f518c1fcf7598
rahsur <rahul.suresh307@gmail.com>
2021-10-23 12:43:40 +0530

    URL update

--- js/index.js
+++ js/index.js
@@ -15,31 +15,31 @@ window.addEventListener("scroll", function () {
 
   
 function sendNotification() {
-  alert("e-Mail Subscription Not yet added.")
-  // var email = document.getElementById("email").value;
-  // if (!isEmpty(email) && validateEmail(email)) {
-  //   document.getElementById("notify").innerHTML = "Subscribing..."
-  //   var json =
-  //   {
-  //     'email': email
-  //   };
+  // alert("e-Mail Subscription Not yet added.")
+  var email = document.getElementById("email").value;
+  if (!isEmpty(email) && validateEmail(email)) {
+    document.getElementById("notify").innerHTML = "Subscribing..."
+    var json =
+    {
+      'email': email
+    };
 
-  //   var xhttp = new XMLHttpRequest();
-  //   xhttp.onreadystatechange = function () {
-  //     if (this.readyState == 4 && this.status == 200) {
-  //       document.getElementById("notify").innerHTML = "Thankyou!";
-  //       backTotext();
-  //     }
-  //     document.getElementById("email").value = "";
-  //   };
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("notify").innerHTML = "Thank you :) ";
+        backTotext();
+      }
+      document.getElementById("email").value = "";
+    };
 
-  //   xhttp.open("POST", "https://quatre-labs-api.herokuapp.com/api/website/get-notified", true);
-  //   xhttp.setRequestHeader("Content-type", "application/json");
-  //   xhttp.send(JSON.stringify(json));
-  // }
-  // else{
-  //   window.alert("Invalid/empty email can't be accepted!");
-  // }
+    xhttp.open("POST", "http://ec2-13-127-191-145.ap-south-1.compute.amazonaws.com:3000/v1/api/website/general/subscribeuser", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify(json));
+  }
+  else{
+    window.alert("Invalid/empty email can't be accepted!");
+  }
 }
 
 function isEmpty(str) {
@@ -53,11 +53,8 @@ function validateEmail(email) {
 
 function backTotext(){
   setTimeout(()=> {
-    // document.getElementById("contact-submit").innerHTML = "Send Message";
     document.getElementById("notify").innerHTML = "Subscribe";
-    // document.getElementById("contact-submit").style.backgroundColor = "#307EFF";
-    // document.getElementById("contact-submit").style.color="white";
-  }, 5000)
+  }, 10000)
 }
 
 function branch1(){
